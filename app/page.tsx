4ef920f81c1f77274bf37e6d9e2226799f1daaae
yeshey <yesheytenzin0@gmail.com>
2025-01-08 18:02:18 +0600

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,6 @@ const App = () => {
       <h1>Hello</h1>
       <h2>hi</h2>
       <h3>friends</h3>
-      <h4>this is my first project</h4>
-      <h2>new update</h2>
-      <h3>this is change ing</h3>
-      <h2>this second changes for </h2>
-      <h3>ajdsdkajhda</h3>
-      <h4> njnvmvkmml </h4>
-      <h2>asdjasjdaghskdgkas</h2>
-      <h2>sdadasfasf</h2>
       <h1>hello</h1>
     </div>
   )
